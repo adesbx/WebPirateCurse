ad3fbe4dca125bdca7c4ebcb92f3578587ae2680
Valentin Cuzin-Rambaud <valentincuzin@murena.io>
2024-03-25 11:56:22 +0100

    déclencher l'apparition d'une fiole Attention peut-être vérifier dans ZRR + pas au même endroit qu'un autre On branch TP3 Changes to be committed: 	modified:   data/data.json 	modified:   routes/admin.route.js 	modified:   services/admin.js

--- api/services/admin.js
+++ api/services/admin.js
@@ -38,6 +38,54 @@ function setZRR(dataZRR) {
   });
 }
 
+
+function spawnFlask(dataFlask) {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./data/data.json', 'utf8', (err, data) => {
+      if (err) {
+        throw new Error(400);
+      }
+      try {
+        const existingData = JSON.parse(data);
+        existingData.push(dataFlask);
+        const jsonDataFlask = JSON.stringify(existingData, null, 4);
+        fs.writeFile('./data/data.json', jsonDataFlask, 'utf8', (err) => {
+            if (err) {
+                console.error('Erreur écriture dans le fichier :', err);
+                reject(err);
+            }
+        });
+        resolve();
+      } catch(error) {
+        reject(error);
+      }
+    });
+  });
+}
+
+
+function newFlaskId() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./data/data.json', 'utf8', (err, data) => {
+      if (err) {
+        throw new Error(400);
+      }
+      try {
+        let flasks = JSON.parse(data)
+        // Filtrer les objets de rôle "FLASK" et extraire leurs ID
+        let flaskIds = flasks.filter(item => item.role === 'FLASK').map(item => item.id);
+
+        // Extraire les nombres des ID et trouver le dernier nombre
+        let numbers = flaskIds.map(id => parseInt(id.match(/\d+/)[0])).sort((a, b) => a - b);
+        let lastNumber = numbers.length > 0 ? numbers[numbers.length - 1] : 0;
+        resolve(lastNumber + 1);
+      } catch(error) {
+        reject(error);
+      }
+    });
+  });
+}
+
 function verifyRole(login, origin) {
   return new Promise(async (resolve, reject) => {
     await axios.get(`http://localhost:8080/users/${login}`, {
@@ -104,6 +152,92 @@ export async function postInitZRR(options, origin, token) {
    } else {
      throw new Error(403)
    }
+  } catch(error) {
+    let statusCode = parseInt(error.message);
+    switch(statusCode){
+      case 401:
+        return {
+          status: '401',
+          data: 'Authentification failed'
+        };
+      case 403: {
+        return {
+          status: '403',
+          data: 'Pas authorisé'
+        };
+      }
+      case 404:
+        return {
+          status: '404',
+          data: 'User pas trouvé'
+        };
+      default:
+        return {
+          status: '400',
+          data: error.message
+        };
+    }
+  }
+}
+
+export async function putTTL(options, origin, token) {
+  try {
+    const login = await authenticate(token, origin);
+    if(verifyRole(login, origin)) {
+
+    }
+  } catch(error) {
+    let statusCode = parseInt(error.message);
+    switch(statusCode){
+      case 401:
+        return {
+          status: '401',
+          data: 'Authentification failed'
+        };
+      case 403: {
+        return {
+          status: '403',
+          data: 'Pas authorisé'
+        };
+      }
+      case 404:
+        return {
+          status: '404',
+          data: 'User pas trouvé'
+        };
+      default:
+        return {
+          status: '400',
+          data: error.message
+        };
+    }
+  }
+}
+
+
+export async function postSpawnFlask(options, origin, token) {
+  try {
+    const login = await authenticate(token, origin);
+    if(verifyRole(login, origin)) {
+      const newId = await newFlaskId();
+      const dataFlask = {
+          "id": "potion"+newId,
+          "position": [
+              options.latLng[0],
+              options.latLng[1]
+          ],
+          "role": "FLASK",
+          "ttl": 60,
+          "potions": 0,
+          "terminated": 0,
+          "turned": 0
+      };
+      await spawnFlask(dataFlask);
+      return {
+        status: '204',
+        data: 'nouvelle potion créer'
+      };
+    }
   } catch(error) {
     let statusCode = parseInt(error.message);
     switch(statusCode){
